601d2e27db9f2be5f30edd62d7991fe121742250
Nyagol <nyagolpeter@gmail.com>
2023-10-05 18:06:29 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// multiply
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+console.log(multiply)
+
+// random
+
+let number = 1
+let random = number
+console.log(number)
+
+// mod
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3%num4
+
+// variable returning max
+
+let numbers = [10, 15, 20, 5, 12];
+let max = Math.max(...numbers);
